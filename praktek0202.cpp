12e34600ae647256214afd7f6418c6c3c28c9e15
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2017-10-20 01:26:54 +0800

    Output bermacam gaya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int main(){
 
-	int niali=0;
+	int nilai=0;
 	int Nilai=1;
 	int NILAI=2;
 	int niLAi=3;
 	
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
+	
+	cout <<nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAi<<endl;
+	cout << angka <<"\n";
+	cout << Angka <<endl<<ANGKA;
 	
 }
